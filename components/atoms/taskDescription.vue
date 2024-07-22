5ec9891e30450e642a1b0b290e4675af81c2415e
ArtuTerra <arthur.terra@hotmai.com>
2024-07-21 22:27:46 -0300

    Criando o store da lista de tarefas

---
+++ components/atoms/taskDescription.vue
@@ -0,0 +1,29 @@
+<template>
+	<h6 class="task__list__box__description">{{ description }}</h6>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+
+export default defineComponent({
+	name: "TaskDescription",
+	props: {
+		description: {
+			type: String as PropType<string>,
+			required: true,
+		},
+	},
+});
+</script>
+
+<style scoped lang="scss">
+.task__list__box__description {
+	margin-top: 5px;
+	height: 130px;
+	max-width: 200;
+	text-wrap: wrap;
+	color: var(--brand-color-primary-400);
+	overflow: hidden;
+	text-overflow: ellipsis;
+}
+</style>
